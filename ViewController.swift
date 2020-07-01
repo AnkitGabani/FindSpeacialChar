efbc6d2c48e432413b536b83021bf0e4039a93eb
Ankit Gabani <31876788+AnkitGabani@users.noreply.github.com>
2020-07-01 20:59:24 +0530

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -27,5 +27,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var dateString = ""
+        if let createDateStr = "2020-07-01T11:09:32.2124188-04:00"
+        {
+                let arrSplit = createDateStr.components(separatedBy: "T")
+                let objct1 = arrSplit[0] as! String
+                let objct2 = arrSplit[1] as! String
+
+                let arrSplit1 = objct1.components(separatedBy: "-")
+                let strYear = arrSplit1[0] as! String
+                let strMonth = arrSplit1[1] as! String
+                let strDay = arrSplit1[2] as! String
+            
+                let arrSplit2 = objct2.components(separatedBy: ":")
+                let strHour = arrSplit2[0] as! String
+                let strMin = arrSplit2[1] as! String
+                
+                let monthNumber = (Int)(strMonth)
+
+            let fmt = DateFormatter()
+            //fmt.dateFormat = "MMM"
+            fmt.dateStyle = .medium
+            let strMonthName = fmt.monthSymbols[monthNumber! - 1]
+            
+            dateString = String.init(format: "%@ %@ %@ %@:%@", strDay,strMonthName.prefix(3) as CVarArg,strYear,strHour,strMin)
+        }
+        
+        cell.lblOrderDate.text = dateString
 
 }
